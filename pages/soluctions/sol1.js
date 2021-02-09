b550da00f3fe83c84e4d07bc2cd76fc196241172
Matteo <matteo16.martini@gmail.com>
2021-02-09 16:59:35 +0100

    gioco gatto

--- pages/soluctions/sol1.js
+++ pages/soluctions/sol1.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -11,12 +11,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          <p>Ecco il gioco! Trova il gatto nelle seguenti foto!</p>
+          <p>Era semplicissimo</p>
         </h1>
-        <img src="/images/Quiz 1.jpg" alt="Quiz uno"  />
-        <p><a href="soluctions/sol1"> Clicca qui per la soluzione!</a></p>
-        <img src="/images/Quiz 2.jpg" alt="Quiz due"  />
-        <p><a href="soluctions/sol2"> Clicca qui per la soluzione! </a></p>
+        <img src="/images/Sol quiz 1.jpg" alt="Sol Quiz uno"  />
       </main>
 
       <footer className={styles.footer}>
